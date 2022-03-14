a39d423940f526b63472529b4a922be8ef902a16
Prince Lal(202405) <cipetprincelal@gmail.com>
2022-03-15 00:07:36 +0530

    Lab-3

--- Lab-3.cpp
+++ Lab-3.cpp
@@ -3,15 +3,16 @@ using namespace std;
 
 double power(double m, int n=2){
     double res=1;
-    for(n>0;n--);
-    res=m*res;//res*=m;
+    for(n>0;n--;)
+        res=m*res;//res*=m;
     return res;
 }
 int main()
 {
-    int n=0, double m=0;
+    int n=0; double m=0;
     cout<<"Enter the value of M: ";
     cin>>m;
-    cout<<m<<"to the power of "<<n<<"is: "<<power(m,n);
+    //cout<<m<<"to the power of "<<n<<"is: "<<power(m,n);
+    cout<<"The square of M "<<power(m);
     return 0;
 }
