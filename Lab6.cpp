78cda12d7910d7f08afacb76b79c2e9e2b212537
Prince Lal(202405) <91109617+PrinceLal-LE@users.noreply.github.com>
2022-04-13 23:02:42 +0530

    Lab6.cpp

--- Lab6.cpp
+++ Lab6.cpp
@@ -1,10 +1,8 @@
 //Writing a class STRING that can be used to store strings, add strings, equate string, output strings.
-
 #include<iostream>
 using namespace std;
 class Str
 {
-   
  public:
   char * str=NULL;
   int len;   //  Using this to track the size of the used length of the string.
@@ -17,7 +15,6 @@ class Str
      i++;
     }
 }
-	
    Str(const char * string)
 {
     len =0;
@@ -28,7 +25,6 @@ class Str
      str[i]=string[i];
     }
 }
-
    bool equate(Str str2)
 {
        if(len !=str2.len)
@@ -47,25 +43,20 @@ class Str
     cout<<str[i++];
     cout<<endl;
 }
-
    void add(Str &str2)
 {
     char *newstr=new char[len+str2.len-1];
     int i=0;
     for(i=0;i<len-1;i++)
     newstr[i]=str[i];
-   
-   for(int j=i;j<i+str2.len;j++)
+    for(int j=i;j<i+str2.len;j++)
    {
        newstr[j]=str2.str[j-i];
    }
    delete []str;
    str =newstr;
 }
-   
 };
-
-
 int main()
 {
     Str str1="Prince ";
@@ -77,7 +68,6 @@ else
  {
     cout<<"Both strings are not same\n";
  }
-
  str1.add(str2);
  str1.display();
  return 0;
