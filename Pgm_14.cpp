e118867079ab21f63f5719a3ed38b2d620f3b164
Prince Lal(202405) <91109617+PrinceLal-LE@users.noreply.github.com>
2022-05-29 23:51:58 +0530

    Pgm-14

---
+++ Pgm_14.cpp
@@ -0,0 +1,32 @@
+// Program to show multilevel inhertitance
+#include<iostream>
+using namespace std;
+class shoe
+{
+    public:
+        Khadims()
+        {
+            cout<<"Khadims is one of the famous brand in shoe. "<<endl;
+        }
+};
+class Redchief : public Khadims
+{
+    public: 
+        Redchief()
+        {
+            cout<<"Redchief is famous for making leather shoes. "<<endl;
+        }
+};
+class Bata : public Redchief
+{
+    public:
+        Bata()
+        {
+            cout<<"Bata is one of the oldest shoe company. "<<endl;
+        }
+};
+int main(int argc, char const *argv[])
+{
+    Bata b1;
+    return 0;
+}
